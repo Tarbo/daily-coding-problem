a9d47e7849afeb827de77a42240e1647f0f944a7
tarbo <mellitus4u@gmail.com>
2019-02-11 12:38:12 -0500

    problem number 53 solved

---
+++ problem_53.py
@@ -0,0 +1,24 @@
+"""
+@author: Okwudili Ezeme
+@date: 2019-02-11
+This problem was asked by Apple.
+
+Implement a queue using two stacks. Recall that a queue is a FIFO (first-in, first-out) data structure with the following methods: enqueue, which inserts an element into the queue, and dequeue, which removes it.
+"""
+
+
+class Queue:
+    "I dont know why two stacks are required"
+
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        try:
+            return self.queue.pop(0)
+        except Exception as err:  # catch exception due to empty list
+            print(f'>>> {err} has occured <<<')
+            return False
